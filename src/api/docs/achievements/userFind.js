0823447a380b6df4d5b72324202b8db246e4b5bd
Taylor Blau <me@ttaylorr.com>
2015-06-05 22:56:45 -0400

    Find a common ancestor between both achievement methods

---
+++ src/api/docs/achievements/userFind.js
@@ -0,0 +1,12 @@
+import AbstractAchievementMethod from "./achievementMethod.js";
+
+export default class UserProgressMethod extends AbstractAchievementMethod {
+  uri() { return "/api/v1/users/:id/achievements"; }
+  description() {
+    return [
+      "This lists all achievements and the user's progress on those achievements.",
+      "Achievements are a resource in themselves, then the user can gain Achievement Earnings, which record the user's progress and earned status on each achievement. Note that an Earning record will not exist if the user has not made progress on an achievement.",
+      "In this endpoint, the difference is not apparent, but for other endpoints this is something you'll need to keep in mind."
+    ].join("\n");
+  }
+}
