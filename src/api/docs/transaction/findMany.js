ff1467e680c753bb346bc13500a0c8003baf06ef
Taylor Blau <me@ttaylorr.com>
2015-07-04 14:55:36 -0400

    add documentation for transaction endpoints

---
+++ src/api/docs/transaction/findMany.js
@@ -0,0 +1,38 @@
+import PaginatedMethod from "../../paginatedMethod.js";
+import Parameter from "../../parameter.js";
+import Example from "../../example.js";
+
+import AccessDeniedResult from "../../accessDeniedResult.js";
+
+export default class FindManyTransactionMethod extends PaginatedMethod {
+  uri() { return "/api/v1/users/:idtransactions"; }
+  version() { return 1; }
+  httpMethod() { return "GET"; }
+  description() { return "Finds information about a single user's transactions."; }
+  parameters() { return super.parameters().concat([ new IDParameter() ]); }
+  examples() { return [ new SuccesfulExample(), new AccessDeniedResult() ]; }
+}
+
+class IDParameter extends Parameter {
+  name() { return "id"; }
+  description() { return "Numeric user ID for which to gather transactions."; }
+}
+
+class SuccesfulExample extends Example {
+  httpCode() { return 200; }
+  data() {
+    return [{
+      createdAt: Date.now().toISOString(),
+      currency_delta: -7.99,
+      gateway: "internal",
+      id: 9,
+      note: "Purchase of the Premium role.",
+      points_delta: 0,
+      promotion: null,
+      reference: "",
+      short_name: "Beam Subscription",
+      status: "pending",
+      user: 2
+    }];
+  }
+}
---
+++ src/api/docs/transaction/findOne.js
@@ -0,0 +1,40 @@
+import Method from "../../method.js";
+import Parameter from "../../parameter.js";
+import Example from "../../example.js";
+
+import AccessDeniedResult from "../../accessDeniedResult.js";
+import ResourceMissingResult from "../../resourceMissingResult.js";
+
+export default class FindOneTransactionMethod extends Method {
+  uri() { return "/api/v1/transactions/:id"; }
+  version() { return 1; }
+  httpMethod() { return "GET"; }
+  description() { return "Finds information about a single transaction."; }
+  parameters() { return [ new IDParameter() ]; }
+  examples() { return [ new SuccesfulExample(), new AccessDeniedResult(),
+                        new ResourceMissingResult("Transaction") ]; }
+}
+
+class IDParameter extends Parameter {
+  name() { return "id"; }
+  description() { return "Numeric user ID of the transaction to get."; }
+}
+
+class SuccesfulExample extends Example {
+  httpCode() { return 200; }
+  data() {
+    return {
+      createdAt: Date.now().toISOString(),
+      currency_delta: -7.99,
+      gateway: "internal",
+      id: 9,
+      note: "Purchase of the Premium role.",
+      points_delta: 0,
+      promotion: null,
+      reference: "",
+      short_name: "Beam Subscription",
+      status: "pending",
+      user: 2
+    }
+  }
+}
