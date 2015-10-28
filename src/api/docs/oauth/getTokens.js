bfd4393459d1d839595266ede172f0af67c4df3d
Jamy Timmermans <me@jamy.be>
2015-10-28 01:52:49 +0100

    Added /oauth/tokens

---
+++ src/api/docs/oauth/getTokens.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Method from "../../method.js";
+import Parameter from "../../parameter.js";
+import Example from "../../example.js";
+
+import TokenResponse from "./tokenResponse.js";
+
+export default class getOauthTokensMethod extends Method {
+    uri() { return "/api/v1/oauth/token"; }
+    version() { return 1; }
+    httpMethod() { return "POST"; }
+    group() { return "oauth" }
+    description() {
+        return (
+            <p>
+                This endpoint returns a fully populated OAuth client.
+                This endpoint will only return public data for the OAuth app.
+            </p>
+        )
+    }
+
+    parameters() { return [
+        new GrantTypeParameter(),
+        new ClientIDParameter(),
+        new RefreshTokenParameter(),
+        new CodeParameter(),
+        new RedirectURIParameter(),
+        new ClientSecretParameter()
+    ]; }
+    examples() { return [ new TokenResponse(), new ErrorfulResult() ]; }
+}
+
+class GrantTypeParameter extends Parameter {
+    name() { return "grant_type"; }
+    description() { return "The type of OAuth grant that is being used. Select from: 'authorization_code' or 'from_refresh'"; }
+    type() { return "body"; }
+}
+
+class ClientIDParameter extends Parameter {
+    name() { return "client_id"; }
+    description() { return "The OAuth client's id string."; }
+    type() { return "body"; }
+}
+
+class RefreshTokenParameter extends Parameter {
+    name() { return "refresh_token"; }
+    description() { return "(Only if (grant_type == 'from_refresh)') The refresh token you want to generate new tokens from."; }
+    type() { return "body"; }
+}
+
+class CodeParameter extends Parameter {
+    name() { return "code"; }
+    description() { return "(Only if (grant_type == 'authorization_code)') The auth code you want to generate tokens from."; }
+    type() { return "body"; }
+}
+
+class RedirectURIParameter extends Parameter {
+    name() { return "redirect_uri"; }
+    description() { return "(Only if (grant_type == 'authorization_code)') The redirect URI for your app's requests."; }
+    type() { return "body"; }
+}
+
+class ClientSecretParameter extends Parameter {
+    name() { return "client_secret"; }
+    description() { return "The OAuth client's secret key."; }
+    type() { return "body"; }
+}
+
+class ErrorfulResult extends Example {
+    httpCode() { return 404; }
+    data() { return "Client not found."; }
+}
