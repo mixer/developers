8a3d71bd80e89ff16062f2fc6d9dc18111b9a9ca
Simon Schick <demwizzy@gmail.com>
2016-06-15 22:08:57 +0200

    start of other tutorial and tabs

---
+++ src/pug/guides/code/python/chat/1.py
@@ -0,0 +1,12 @@
+# Address of the beam website.
+# No trailing slash.
+beam_addr = 'https://beam.pro'
+
+# Username of the account.
+username = 'user123'
+
+# Password of the account.
+password = 'password'
+
+# The id of the channel you want to connect to.
+channel = 12345
