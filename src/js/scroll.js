f81b2a15c416ab7f2d07b8c17198b7aea98ff5ae
Connor Peet <connor@peet.io>
2016-06-22 18:22:22 -0700

    WIP on fancier scrollSpy

---
+++ src/js/scroll.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.rest-sidebar').each(function (i, el) {
+        var json = el.dataset.scrollspy;
+        var opts = json ? JSON.parse(json) : { container: 'body' };
+        opts.target = '.rest-sidebar';
+        $(opts.container).scrollspy(opts);
+    });
+
+    $('.rest-sidebar').each(function (i, el) {
+        var json = el.dataset.scrollStick;
+        var defaults = { parent: '.site-content', recalc_every: 500 };
+        $(el).stick_in_parent(json ? JSON.parse(json) : defaults);
+    });
+});
