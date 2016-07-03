6ccee47a10ec51d04b7bb1e60f8c4728201589a9
Richard Fox <prime@ahref.co.uk>
2016-07-03 05:28:54 +0100

    Reduce repetition

--- src/tutorials/code/node/interactive/4.js
+++ src/tutorials/code/node/interactive/4.js
@@ -10,14 +10,14 @@ function performRobotHandShake (robot) {
 }
 
 function setupRobotEvents (robot) {
-    function setupRobotEvents (robot) {
     robot.on('report', report => {
         const mouse = rjs.getMousePos();
         if (report.joystick.length > 0) {
-            if (!isNaN(report.joystick[0].coordMean.x) && !isNaN(report.joystick[0].coordMean.y)) {
+            const mean = report.joystick[0].coordMean;
+            if (!isNaN(mean.x) && !isNaN(mean.y)) {
                 rjs.moveMouse(
-                    Math.round(mouse.x + 300 * report.joystick[0].coordMean.x),
-                    Math.round(mouse.y + 300 * report.joystick[0].coordMean.y)
+                    Math.round(mouse.x + 300 * mean.x),
+                    Math.round(mouse.y + 300 * mean.y)
                 );
             }
         }
