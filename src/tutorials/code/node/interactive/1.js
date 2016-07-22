fa383452fc8284abfe3256db0fb61e218520667d
Richard Fox <prime@ahref.co.uk>
2016-07-22 23:31:56 +0100

    Interactive

--- src/tutorials/code/node/interactive/1.js
+++ src/tutorials/code/node/interactive/1.js
@@ -1,5 +1,5 @@
 const Beam = require('beam-client-node');
-const Interative = require('beam-interactive-node');
+const Interactive = require('beam-interactive-node');
 const rjs = require('robotjs');
 
 const channelId = 1234;
