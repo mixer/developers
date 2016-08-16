2330588b629adb4b3545da34b1e52fceb7979a61
Jayden Bailey <jaydenkieran@gmail.com>
2016-08-16 16:03:23 +0100

    This if statement should also be indented

--- src/tutorials/code/python/chat/5.py
+++ src/tutorials/code/python/chat/5.py
@@ -6,7 +6,7 @@ from functools import partial
 
 
 if __name__ == "__main__":
-chat = create(config)
+    chat = create(config)
 
 # Tell chat to authenticate with the beam server. It'll throw
 # a chatty.errors.NotAuthenticatedError if it fails.
