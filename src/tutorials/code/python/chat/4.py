bcee04693b07688560d9e83506a1ef124ab1438e
2Cubed <2Cubed@users.noreply.github.com>
2016-10-28 02:33:58 -0400

    Update 4.py to include changes

--- src/tutorials/code/python/chat/4.py
+++ src/tutorials/code/python/chat/4.py
@@ -1,5 +1,9 @@
+# Handle incoming messages.
+def on_message(message):
+    print("RECEIVE:", message)
+
 # Listen for incoming messages. When they come in, just print them.
-chat.on("message", partial(print, "RECEIVE:"))
+chat.on("message", on_message)
 
 # Create a timer that sends the message "Hi!" every second.
 PeriodicCallback(
