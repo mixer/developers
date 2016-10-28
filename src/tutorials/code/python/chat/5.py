87e5d0e5e92ed5f94e4238e73453934abc7835dd
2Cubed <2Cubed@users.noreply.github.com>
2016-10-28 02:31:05 -0400

    Replace partial with a function definition
    Fix indentation, as well.

--- src/tutorials/code/python/chat/5.py
+++ src/tutorials/code/python/chat/5.py
@@ -2,24 +2,27 @@ from chatty import create
 import config
 
 from tornado.ioloop import PeriodicCallback, IOLoop
-from functools import partial
 
 
 if __name__ == "__main__":
     chat = create(config)
 
-# Tell chat to authenticate with the beam server. It'll throw
-# a chatty.errors.NotAuthenticatedError if it fails.
-chat.authenticate(config.CHANNEL)
+    # Tell chat to authenticate with the beam server. It'll throw
+    # a chatty.errors.NotAuthenticatedError if it fails.
+    chat.authenticate(config.CHANNEL)
 
-# Listen for incoming messages. When they come in, just print them.
-chat.on("message", partial(print, "RECEIVE:"))
+    # Handle incoming messages.
+    def on_message(message):
+        print("RECEIVE:", message)
+    
+    # Listen for incoming messages. When they come in, just print them.
+    chat.on("message", on_message)
 
-# Create a timer that sends the message "Hi!" every second.
-PeriodicCallback(
-    lambda: chat.message('Hi!'),
-    1000
-).start()
+    # Create a timer that sends the message "Hi!" every second.
+    PeriodicCallback(
+        lambda: chat.message('Hi!'),
+        1000
+    ).start()
 
-# Start the tornado event loop.
-IOLoop.instance().start()
+    # Start the tornado event loop.
+    IOLoop.instance().start()
