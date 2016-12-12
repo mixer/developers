11b2e101a1a22f4c384d57e5644e127216032823
Richard Fox <prime@ahref.co.uk>
2016-12-12 12:37:07 +0100

    Fix getting mouse locked to top right when no players

--- src/tutorials/code/java/interactive/3.java
+++ src/tutorials/code/java/interactive/3.java
@@ -5,10 +5,12 @@ robot.on(Protocol.Report.class, report -> {
         Protocol.Coordinate coordMean = report.getJoystick(0).getCoordMean();
         Point mousePosition = MouseInfo.getPointerInfo().getLocation();
 
-        // Apply it to the current mouse position
-        controller.mouseMove(
+        // Apply it to the current mouse position, if its values are not NaN
+        if (!Double.isNaN(coordMean.getX()) && !Double.isNaN(coordMean.getY())) {
+            controller.mouseMove(
                 ((int) (mousePosition.getX() + 300 * coordMean.getX())),
                 ((int) (mousePosition.getY() + 300 * coordMean.getY()))
-        );
+            );
+        }
     }
 });
--- src/tutorials/code/java/interactive/4.java
+++ src/tutorials/code/java/interactive/4.java
@@ -20,15 +20,22 @@ public class Main {
                     .channel(1234).build(beam).get();
 
             robot.on(Protocol.Report.class, report -> {
+                // If we have any joysticks in the report
                 if (report.getJoystickCount() > 0) {
+                    // Get the coordMean from the joystick
                     Protocol.Coordinate coordMean = report.getJoystick(0).getCoordMean();
                     Point mousePosition = MouseInfo.getPointerInfo().getLocation();
-                    controller.mouseMove(
+
+                    // Apply it to the current mouse position, if its values are not NaN
+                    if (!Double.isNaN(coordMean.getX()) && !Double.isNaN(coordMean.getY())) {
+                        controller.mouseMove(
                             ((int) (mousePosition.getX() + 300 * coordMean.getX())),
                             ((int) (mousePosition.getY() + 300 * coordMean.getY()))
-                    );
+                        );
+                    }
                 }
             });
+
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
