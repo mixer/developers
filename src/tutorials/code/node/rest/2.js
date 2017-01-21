419909fc18a3df31e3e0ee710c8652122b142f4f
Richard Fox <richard.fox@ahref.co.uk>
2017-01-21 22:57:06 +0100

    Wait this doesnt actually need auth

--- src/tutorials/code/node/rest/2.js
+++ src/tutorials/code/node/rest/2.js
@@ -4,14 +4,9 @@ const Beam = require('beam-client-node');
 
 const beam = new Beam();
 
-beam.use('oauth', {
-    tokens: {
-        access: 'AUTH_TOKEN',
-        expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
-    },
-});
+const channelName = process.argv[2];
 
-client.request('GET', `users/current`)
+beam.request('GET', `channels/${channelName}`)
 .then(res => {
     const viewers = res.body.channel.viewersTotal;
     console.log(`You have ${viewers} total viewers...`);
--- src/tutorials/code/node/rest/5.js
+++ src/tutorials/code/node/rest/5.js
@@ -1,19 +1,12 @@
 'use strict';
 
 const Beam = require('beam-client-node');
-const username = process.argv[2];
-const password = process.argv[3];
 
 const beam = new Beam();
 
-beam.use('oauth', {
-    tokens: {
-        access: 'AUTH_TOKEN',
-        expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
-    },
-});
+const channelName = process.argv[2];
 
-client.request('GET', `users/current`)
+beam.request('GET', `channels/${channelName}`)
 .then(res => {
     const viewers = res.body.channel.viewersTotal;
     console.log(`You have ${viewers} total viewers...`);
