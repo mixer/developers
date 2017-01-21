053fcf0d5d8ebe9fbf8c8a775943f0c3f047d75f
Richard Fox <richard.fox@ahref.co.uk>
2017-01-21 22:06:32 +0100

    OAuth the rest js tutorial

--- src/tutorials/code/node/rest/2.js
+++ src/tutorials/code/node/rest/2.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const Beam = require('beam-client-node');
-const username = process.argv[2];
-const password = process.argv[3];
 
 const beam = new Beam();
 
-beam.use('password', {
-    username,
-    password,
-})
-.attempt()
+beam.use('oauth', {
+    tokens: {
+        access: 'AUTH_TOKEN',
+        expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
+    },
+});
+
+client.request('GET', `users/current`)
 .then(res => {
     const viewers = res.body.channel.viewersTotal;
     console.log(`You have ${viewers} total viewers...`);
--- src/tutorials/code/node/rest/5.js
+++ src/tutorials/code/node/rest/5.js
@@ -6,11 +6,14 @@ const password = process.argv[3];
 
 const beam = new Beam();
 
-beam.use('password', {
-    username,
-    password,
-})
-.attempt()
+beam.use('oauth', {
+    tokens: {
+        access: 'AUTH_TOKEN',
+        expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
+    },
+});
+
+client.request('GET', `users/current`)
 .then(res => {
     const viewers = res.body.channel.viewersTotal;
     console.log(`You have ${viewers} total viewers...`);
