1041a61f3c36a6cdb376e8a9165859689ffb025f
Richard Fox <richard.fox@ahref.co.uk>
2017-02-07 15:41:09 +0100

    >.<

--- src/js/oauth.js
+++ src/js/oauth.js
@@ -34,8 +34,7 @@
         },
         openImplicitOAuthWindow: function (scopes) {
             var clientId = 'fa54866255ea641235e596e5659fa726a4aa9f7ecc72758f';
-            //var redirectURI = 'https://dev.beam.pro/oauthreturn.html';
-            var redirectURI = 'http://localhost:3000/oauthreturn.html';
+            var redirectURI = 'https://dev.beam.pro/oauthreturn.html';
             var url = 'https://beam.pro/oauth/authorize?response_type=token&' +
                 'redirect_uri=' + redirectURI + '&' +
                 'scope=' + scopes + '&' +
